2e08f8e43ee5b12319e36f6572d6017e85caca34
shobah24 <shokran.bahram14@gmail.se>
2025-05-13 15:09:10 +0200

    Fixed Dbcontext. Parkodning

---
+++ restortlibrary/Data/ResortContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restortlibrary.Models;
+
+namespace restortlibrary.Data
+{
+    public class ResortContext : DbContext
+    {
+        public ResortContext(DbContextOptions<ResortContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Accessibility> Accessibilities { get; set; }
+        public DbSet<Accomodation> Accommodations { get; set; }
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<PriceChanges> PriceChanges { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
