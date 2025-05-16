fe5d1df30ce8ea41fbd03b56eefbd5cef695f324
johan <johan@LAPTOP-6KO4S5LR>
2025-05-16 15:16:54 +0200

    Added tests to BookingFactory

--- restortlibrary/Factories/IFactories/IBookingFactory.cs
+++ restortlibrary/Factories/IFactories/IBookingFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using restortlibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace restortlibrary.Factories.IFactories
 {
     public interface IBookingFactory
     {
+        Booking CreateBooking(DateTime checkIn, DateTime checkOut, Customer customer, Accomodation accomodation);
     }
 }
