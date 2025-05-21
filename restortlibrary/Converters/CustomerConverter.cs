02d9e1dec1c8d18209efb64c6ce40298704da502
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-05-21 10:31:38 +0200

    Made an example flow of controller request -> dto conversion (via factory) -> adding to database
    (Create Customer endpoint)

---
+++ restortlibrary/Converters/CustomerConverter.cs
@@ -0,0 +1,40 @@
+﻿using restortlibrary.Factories.IFactories;
+using restortlibrary.Models;
+using restortlibrary.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restortlibrary.Converters
+{
+    public class CustomerConverter : IConverter<Customer, CreateCustomerRequestDTO>
+    {
+        private readonly ICustomerFactory _factory;
+
+        public CustomerConverter(ICustomerFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public Customer FromDTOtoObject(CreateCustomerRequestDTO entity)
+        {
+            return _factory.CreateCustomer("Basic", entity.FirstName, entity.LastName, entity.Email, entity.PhoneNumber, entity.PaymentMethod);
+
+        }
+
+        public CreateCustomerRequestDTO FromObjecttoDTO(Customer entity)
+        {
+            return new CreateCustomerRequestDTO()
+            {
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                Email = entity.Email,
+                PhoneNumber = entity.Phone,
+                PaymentMethod = entity.PaymentMethod
+            };
+        }
+
+    }
+}
