c5b3588178f0f1b7f610277a38db87a915d4113f
shobah24 <shokran.bahram14@gmail.se>
2025-05-21 20:55:00 +0200

    dto for guests in booking

---
+++ resortdtos/GuestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resortdtos
+{
+    public class GuestDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
