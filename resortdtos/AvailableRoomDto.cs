88689740a7eb47ac455397ff81dcf1c06cabae18
shobah24 <shokran.bahram14@gmail.se>
2025-05-21 20:42:58 +0200

    Dto for available rooms

---
+++ resortdtos/AvailableRoomDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resortdtos
+{
+    public class AvailableRoomDto
+    {
+        public int Id { get; set; }
+        public string AccomodationType { get; set; }
+        public string? Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int MaxOccupancy { get; set; }
+        public decimal BasePrice { get; set; }
+        public ICollection<AccessibilityDto> Accessibility { get; set; } = new List<AccessibilityDto>();
+
+    }
+}
