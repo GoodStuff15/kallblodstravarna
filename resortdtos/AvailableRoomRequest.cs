a32f369c3f4363cc740f8260571ba2cda27fecf0
shobah24 <shokran.bahram14@gmail.se>
2025-05-21 20:25:31 +0200

    available room Request(Dto)

---
+++ resortdtos/AvailableRoomRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resortdtos
+{
+    public class AvailableRoomRequest
+    {
+        [Required]
+        public DateTime CheckIn { get; set; }
+        [Required]
+        public DateTime CheckOut { get; set; }
+
+    }
+}
