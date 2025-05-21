fc29b67cbc5b073a10181c7728e71cabf1932fd8
shobah24 <shokran.bahram14@gmail.se>
2025-05-21 21:13:12 +0200

    Dto for booking overview- for owner

---
+++ resortdtos/BookingsOverviewDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resortdtos
+{
+    public class BookingsOverviewDto
+    {
+        public int BookingId { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int AccomodationId { get; set; }  
+        public int CustomerId { get; set; }
+        public bool Active { get; set; } = true;
+    }
+}
