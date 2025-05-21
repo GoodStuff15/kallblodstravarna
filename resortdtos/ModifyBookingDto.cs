3959e708668ff39c5a42a9ebe8e7a83df9c118ff
shobah24 <shokran.bahram14@gmail.se>
2025-05-21 21:21:50 +0200

    Dto for modify a booking- for receptionist

---
+++ resortdtos/ModifyBookingDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resortdtos
+{
+    public class ModifyBookingDto
+    {
+        public int BookingId { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int AccomodationId { get; set; }
+        public ICollection<GuestDto> Guests { get; set; } = new List<GuestDto>(); 
+        public ICollection<int> AdditionalOptionIds { get; set; } = new List<int>();
+    }
+}
