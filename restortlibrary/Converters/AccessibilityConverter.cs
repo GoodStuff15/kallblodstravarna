9673559bb7ff0403cc9cd81ba0c3ba9a8c72dda2
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2025-05-22 20:44:11 +0200

    Added test converter class
    Finished test + implementation of accessibilityconverter

---
+++ restortlibrary/Converters/AccessibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using resortdtos;
+using restortlibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restortlibrary.Converters
+{
+    public class AccessibilityConverter : IConverter<Accessibility, AccessibilityDto>
+    {
+        public Accessibility FromDTOtoObject(AccessibilityDto dto)
+        {
+            var obj = new Accessibility();
+            obj.Name = dto.Name;
+            obj.Description = dto.Description;
+
+            return obj;
+        }
+
+        public AccessibilityDto FromObjecttoDTO(Accessibility obj)
+        {
+            var dto = new AccessibilityDto();
+            dto.Name = obj.Name;
+            dto.Description = obj.Description;
+
+            return dto;
+        }
+    }
+}
