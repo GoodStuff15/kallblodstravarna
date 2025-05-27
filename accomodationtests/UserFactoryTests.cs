da9c47b1d9c2c87acff68746cd8e87f3a2be4f7a
johan <johan@LAPTOP-6KO4S5LR>
2025-05-27 11:43:49 +0200

    Updated tests for UserBuilder

--- accomodationtests/UserFactoryTests.cs
+++ accomodationtests/UserFactoryTests.cs
@@ -1,5 +1,5 @@
-using resortlibrary.Factories;
-using resortlibrary.Factories.IFactories;
+using resortlibrary.Builders;
+using resortlibrary.Builders.IBuilders;
 using resortlibrary.Models;
 
 namespace accomodationtests;
@@ -7,28 +7,37 @@ namespace accomodationtests;
 [TestClass]
 public class UserFactoryTests
 {
-    private IUserBuilder _factory;
+    private UserBuilder _builder;
 
     [TestInitialize]
     public void Setup()
     {
-        _factory = new UserBuilder();
+        _builder = new UserBuilder();
     }
     [TestMethod]
-    public void CreateUser_ValidData_ShouldReturnUser()
+    public void Build_WithValidData_ShouldReturnUser()
     {
-        var customer = new Customer { 
-            Id = 1, 
+        var customer = new Customer
+        {
+            Id = 1,
             FirstName = "Johan",
             LastName = "Hansson",
             Email = "hasse@hotmail.com",
             Phone = "0707000000",
             PaymentMethod = "Cash",
-            Type = "Premium"        
+            Type = "Premium"
         };
         var expiry = DateTime.UtcNow.AddDays(5);
 
-        var user = _factory.CreateUser("johan", "pass123", "Guest", 1, customer, "token", expiry);
+        var user = _builder
+            .WithUsername("johan")
+            .WithPasswordHash("pass123")
+            .WithRole("Guest")
+            .WithCustomerId(1)
+            .WithCustomer(customer)
+            .WithRefreshToken("token")
+            .WithRefreshTokenExpiry(expiry)
+            .Build();
 
         Assert.IsNotNull(user);
         Assert.AreEqual("johan", user.Username);
@@ -41,23 +50,55 @@ public class UserFactoryTests
     }
 
     [DataTestMethod]
-    [DataRow(null, "password", "User", 1)]
-    [DataRow("", "password", "User", 1)]
-    [DataRow(" ", "password", "User", 1)]
-    [DataRow("user", null, "User", 1)]
-    [DataRow("user", "", "User", 1)]
-    [DataRow("user", " ", "User", 1)]
-    [DataRow("user", "password", null, 1)]
-    [DataRow("user", "password", "", 1)]
-    [DataRow("user", "password", " ", 1)]
-    [DataRow("user", "password", "User", 0)]
-    [DataRow("user", "password", "User", -5)]
-    public void CreateUser_InvalidData_ShouldThrow(string username, string passwordHash, string role, int customerId)
+    [DataRow(null, "password", "User")]
+    [DataRow("", "password", "User")]
+    [DataRow(" ", "password", "User")]
+    [DataRow("user", null, "User")]
+    [DataRow("user", "", "User")]
+    [DataRow("user", " ", "User")]
+    [DataRow("user", "password", null)]
+    [DataRow("user", "password", "")]
+    [DataRow("user", "password", " ")]
+    public void Build_MissingRequiredFields_ShouldThrowInvalidOperationException(string username, string passwordHash, string role)
     {
-        var ex = Assert.ThrowsException<ArgumentException>(() =>
-        _factory.CreateUser(username, passwordHash, role, customerId)
-        );
-        Assert.AreEqual("Fyll i samtliga fält", ex.Message);
+        if (!string.IsNullOrWhiteSpace(username))
+            _builder.WithUsername(username);
+        if (!string.IsNullOrWhiteSpace(passwordHash))
+            _builder.WithPasswordHash(passwordHash);
+        if (!string.IsNullOrWhiteSpace(role))
+            _builder.WithRole(role);
+
+        Assert.ThrowsException<InvalidOperationException>(() => _builder.Build());
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public void WithUsername_InvalidValues_ShouldThrowArgumentException(string username)
+    {
+        var ex = Assert.ThrowsException<ArgumentException>(() => _builder.WithUsername(username));
+        Assert.AreEqual("Användarnamn måste anges.", ex.Message);
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public void WithPasswordHash_InvalidValues_ShouldThrowArgumentException(string passwordHash)
+    {
+        var ex = Assert.ThrowsException<ArgumentException>(() => _builder.WithPasswordHash(passwordHash));
+        Assert.AreEqual("Lösenord måste anges.", ex.Message);
+    }
+
+    [DataTestMethod]
+    [DataRow(null)]
+    [DataRow("")]
+    [DataRow(" ")]
+    public void WithRole_InvalidValues_ShouldThrowArgumentException(string role)
+    {
+        var ex = Assert.ThrowsException<ArgumentException>(() => _builder.WithRole(role));
+        Assert.AreEqual("Roll måste anges.", ex.Message);
     }
     
 }
