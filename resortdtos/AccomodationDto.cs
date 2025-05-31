419b3f638a45692f59d68c9a8639b0fa5bc000c0
shobah24 <shokran.bahram14@gmail.se>
2025-05-31 15:41:54 +0200

    added accomodationDto

---
+++ resortdtos/AccomodationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace resortdtos
+{
+    public class AccomodationDto
+    {
+        //public int Id { get; set; }
+        public string? Name { get; set; }
+        public int MaxOccupancy { get; set; }
+        public int AccomodationTypeId { get; set; }
+        public ICollection<int> AccessibilityIds { get; set; } = new List<int>();
+    }
+}
