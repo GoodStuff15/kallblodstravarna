4ef04bd66d94ac250c7c4c02604c5371b26621cc
shobah24 <shokran.bahram14@gmail.se>
2025-06-02 20:30:24 +0200

    AccomodationTypeDto for controller

---
+++ resortdtos/AccomodationTypeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace resortdtos
+{
+    public class AccomodationTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal BasePrice { get; set; }
+    }
+}
